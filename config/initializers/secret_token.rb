dfc28a429c64da192412c26ef88b4cbf39b4725e
Allan Sechrist <Abyssalspecter@gmail.com>
2014-10-28 11:42:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OwariCosplay::Application.config.secret_key_base = 'b00e8fbf2a5b4e5202a3e814953918c0f27b57d4366a1552e2c013dd91a447b5a857f88b5c433224b2b48290ae3ae19a0ea9a94b1a504bf5d8367cf1cf87e0ed'
